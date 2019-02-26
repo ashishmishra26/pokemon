d707685403e1518c14b2656bc5d407d6013d3eb3
Ashish Mishra <ashish@fusioncharts.com>
2019-02-26 15:29:53 +0530

    component skeleton created

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
+/* eslint-disable no-loop-func */
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import List from './components/List';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      data: {}
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <Header />
+       <List />
       </div>
     );
+  }
+  componentDidMount () {
+    let allPokemons = [];
+    fetch('https://pokeapi.co/api/v2/pokemon')
+    .then(response => response.json())
+    .then(async response => {
+      if (response) {
+        allPokemons = [...response.results];
+        while (response.next) {
+          response = await fetch(response.next).then(response => response.json());
+          response && (allPokemons = [...allPokemons, ...response.results]);
+        }
+      }
+      this.setState({data: allPokemons});
+    })
   }
 }
 
---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Card extends Component {
+  render() {
+    return (
+      <div className="card-container">
+        <div className="card-header"></div>
+        <div className="card-content"></div>
+      </div>
+    )
+  }
+}
